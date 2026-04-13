1a62a7468f9cc7400b9c6657318d4c76862bb653
Eric Sexton <foopis23@gmail.com>
2026-04-13 15:29:07 -0400

    setup express

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Hello world")
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors('*'))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+	res.json({
+		hello: "world"
+	})
+})
+
+app.listen(3000, (err) => {
+	if (err) {
+		console.error(err)
+	} else {
+		console.log("listening at http://localhost:3000")
+	}
+})
